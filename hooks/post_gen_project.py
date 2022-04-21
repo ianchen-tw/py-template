eb4a012bff54e634e3da7ac042e9cdb40b86b6f4
Ian Chen <ianre657@gmail.com>
2022-04-21 12:21:29 +0800

    postgen scripts

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,29 @@
-def main():
-    print("Post run done!!")
+import shutil
+import sys
+from subprocess import run
+from typing import Optional, List
+from pathlib import Path
+
+def require_exe(name: str) -> Path:
+    exe = shutil.which(name)
+    if not exe:
+        print(f"require executable: {exe}")
+        sys.exit(1)
+    return Path(exe)
+
+def do_poetry_install_dependency():
+    poetry = require_exe("poetry")
+    run([poetry,"install", "--no-interaction"])
+
+def do_git_init():
+    git = require_exe("git")
+    run([git, "init"])
+    run([git, "branch", "-m", "master", "main"])
+    run([git, "add", "README.md", ".gitignore"])
+    run([git, 'commit', "-m", "inital commit"]) 
+
     
 if __name__ == "__main__":
-    main()
+    do_poetry_install_dependency()
+    do_git_init()
+    print("Post run done!!")
